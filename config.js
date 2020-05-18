1327a5b83c53b1acff5e3ed9a9ff7eaf95d6ed44
UltraNote <support@ultranote.org>
2020-05-18 14:49:28 +0200

    update

---
+++ config.js
@@ -0,0 +1,13 @@
+var api = "https://infinity.ultranote.org:8119";
+
+var email = "support@ultranote.org";
+var telegram = "https://t.me/ultranote_official";
+var discord = "https://discord.gg/qHSNkH3";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "https://stats.ultranote.org/index.html?hash={id}#blockchain_block";
+var transactionExplorer = "https://stats.ultranote.org/index.html?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/custom.css";
+var defaultLang = 'en';
